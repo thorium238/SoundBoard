bb52ccc0b5bc54a3ea436ab004ad4495a2059f94
Thomas Piotrowski <Thomas.Piotrowski@gmail.com>
2016-09-01 16:46:16 -0500

    Initial

---
+++ Sound Board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Board
+//
+//  Created by Thomas Piotrowski on 9/1/16.
+//  Copyright © 2016 Thomas Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
